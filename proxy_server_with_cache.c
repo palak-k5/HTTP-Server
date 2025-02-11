4f6f526cec2701f0b596fac4bb554e573271dc85
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-02-12 01:06:23 +0530

    connecting to remote server

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -45,6 +45,46 @@ pthread_mutex_t lock;
 
 cache_element* head;                
 int cache_size;             
+int connectRemoteServer(char* host_addr, int port_num)
+{
+	// Creating Socket for remote server ---------------------------
+
+	int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( remoteSocket < 0)
+	{
+		printf("Error in Creating Socket.\n");
+		return -1;
+	}
+	
+	// Get host by the name or ip address provided
+
+	struct hostent *host = gethostbyname(host_addr);	
+	if(host == NULL)
+	{
+		fprintf(stderr, "No such host exists.\n");	
+		return -1;
+	}
+
+	// inserts ip address and port number of host in struct `server_addr`
+	struct sockaddr_in server_addr;
+
+	bzero((char*)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_num);
+
+	bcopy((char *)host->h_addr,(char *)&server_addr.sin_addr.s_addr,host->h_length);
+
+	// Connect to Remote server ----------------------------------------------------
+
+	if( connect(remoteSocket, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr)) < 0 )
+	{
+		fprintf(stderr, "Error in connecting !\n"); 
+		return -1;
+	}
+	// free(host_addr);
+	return remoteSocket;
+}
 
 int handle_request(int clientSocket, ParsedRequest *request, char *tempReq)
 {
@@ -72,6 +112,15 @@ int handle_request(int clientSocket, ParsedRequest *request, char *tempReq)
 		printf("unparse failed\n");
 		//return -1;				// If this happens Still try to send request without header
 	}
+
+
+    int server_port = 80;				// Default Remote Server Port
+	if(request->port != NULL)
+		server_port = atoi(request->port);
+
+	int remoteSocketID = connectRemoteServer(request->host, server_port);
+
+	
 }
 void* thread_fn(void* socketNew)
 {
